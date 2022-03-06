7f2315374628d752bd0c1913979fdecb94640bd9
윤철중 <cjfwnd123zz@gmail.com>
2022-03-06 19:54:23 +0900

    22-03-06

--- BOJ/python/1931.py
+++ BOJ/python/1931.py
@@ -7,21 +7,19 @@ from sys import stdin
 
 input = stdin.readline
 
-N = int(input())
-
 timetable = []
-
-for _ in range(N):
-    timetable.append(list(map(int, input().rsplit())))
+for N in range(int(input())):
+    timetable.append(tuple(map(int, input().rsplit())))
 
 timetable.sort(key=lambda x: (x[1], x[0]))
 
-prev_end = 0
+prev_end = -1
 cnt = 0
-for t in timetable:
-    start, end = t
+for time in timetable:
+    start, end = time
     if prev_end > start:
         continue
-    prev_end = end
     cnt += 1
+    prev_end = end
+
 print(cnt)
