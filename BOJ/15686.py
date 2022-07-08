bd4f83db71c132a658f5063a233da4ad2bbff827
윤철중 <cjfwnd123zz@gmail.com>
2022-07-08 12:08:14 +0900

    22-07-08

--- BOJ/15686.py
+++ BOJ/15686.py
@@ -3,33 +3,33 @@
 https://www.acmicpc.net/problem/15686
 """
 
-from sys import stdin
-from math import inf
+import math
 from itertools import combinations
 
-input = stdin.readline
+INF = math.inf
 
-n, m = map(int, input().rsplit())
-graph = [list(map(int, input().rsplit())) for _ in range(n)]
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
 houses = []
-chikens = []
+restaurants = []
 
 for i in range(n):
     for j in range(n):
         if graph[i][j] == 2:
-            chikens.append((i, j))
+            restaurants.append((i, j))
         if graph[i][j] == 1:
             houses.append((i, j))
 
-min_v = inf
-for combi in combinations(chikens, m):
-    sum_v = 0
+ans = INF
+for combination in combinations(restaurants, m):
+    sum_dist = 0
     for house in houses:
         temp = 100
-        for chiken in combi:
-            dist = abs(chiken[0]-house[0]) + abs(chiken[1]-house[1])
+        for restaurant in combination:
+            dist = abs(restaurant[0] - house[0]) + \
+                abs(restaurant[1] - house[1])
             temp = min(temp, dist)
-        sum_v += temp
-    min_v = min(min_v, sum_v)
+        sum_dist += temp
+    ans = min(ans, sum_dist)
 
-print(min_v)
+print(ans)
