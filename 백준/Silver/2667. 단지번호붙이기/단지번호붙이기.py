ad62eda7057d58980b726a3a11ba5ca76dbabbe1
Yoon Chul Joong <cjfwnd123zz@gmail.com>
2024-08-19 20:12:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 52 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -3,42 +3,36 @@ from collections import deque
 
 input = stdin.readline
 
-N = int(input())
-apartment = []
-visited = [[False for _ in range(N)] for _ in range(N)]
-result = []
+n = int(input())
+graph = [list(map(int, input().rstrip())) for _ in range(n)]
+visited = [[False for _ in range(n)] for _ in range(n)]
+dr, dc = [0, -1, 0, 1], [-1, 0, 1, 0]
 
-def bfs(x, y):
-	q = deque()
-	dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-	q.append([x, y])
-	cnt = 0
-	while len(q) != 0:
-		x, y = q.popleft()
-		if visited[x][y] == True:
-			continue
-		for i in range(4):
-			nx, ny = x + dx[i], y + dy[i]
-			if 0 <= nx < N and 0 <= ny < N and apartment[nx][ny] == 1:
-				q.append([nx, ny])
-		visited[x][y] = True
-		cnt += 1
-		
-	return cnt
+def bfs(start_r, start_c):
+  q = deque()
+  q.append([start_r, start_c])
+  cnt = 1
+  visited[start_r][start_c] = True
+  while len(q):
+    r, c = q.popleft()
+    for i in range(4):
+      nr, nc = dr[i] + r, dc[i] + c
+      if 0 <= nr < n and 0 <= nc < n:
+        if graph[nr][nc] == 1 and not visited[nr][nc]:
+          cnt += 1
+          visited[nr][nc] = True
+          q.append([nr, nc])
+      
+  return cnt
 
-for i in range(N):
-	apartment.append(list(map(int, input().rstrip())))	
-
-for b in range(N):
-	for a in range(N):
-		if visited[a][b] == True:
-			continue
-		if apartment[a][b] == 1:
-			result.append(bfs(a, b))
-
-result.sort()
-
-print(len(result))
-for answer in result:
-	print(answer)
-
+ans = []
+for i in range(n):
+  for j in range(n):
+    if graph[i][j] == 1 and not visited[i][j]:
+      result = bfs(i, j)
+      ans.append(result)
+      
+ans.sort()
+print(len(ans))
+for k in ans:
+  print(k)
