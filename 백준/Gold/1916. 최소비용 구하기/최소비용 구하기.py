f41b1df48547755482f212b6e8952a53668fe83d
ChulJoong <cjfwnd123zz@gmail.com>
2024-09-12 19:22:59 +0900

    [Gold V] Title: 최소비용 구하기, Time: 232 ms, Memory: 46632 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,41 @@
+from sys import stdin
+from collections import defaultdict
+
+input = stdin.readline
+
+n = int(input())
+m = int(input())
+
+routes = defaultdict(list)
+for _ in range(m):
+    a, b, c = map(int, input().rsplit())
+    routes[a].append([b, c])
+
+origin, dest = map(int, input().rsplit())
+
+def get_next_city():
+    min_value = 1e9
+    min_index = 0
+    for i in range(1, n+1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            min_index = i
+
+    return min_index
+
+visited = [False for _ in range(n+1)]
+distance = [1e9 for _ in range(n+1)]
+
+distance[origin] = 0
+
+for i in range(n):
+    city = get_next_city()
+    
+    if city == 0:
+        break
+    
+    visited[city] = True
+    for ncity, cost in routes[city]:
+        distance[ncity] = min(cost + distance[city], distance[ncity])
+
+print(distance[dest])
